a95a676009cd033d4a53443d917074a23b5c928f
andre <andre4130@gmail.com>
2020-12-08 23:22:00 +0100

    added setState functions in the searchbar

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -2,18 +2,56 @@ import React, { useState, useEffect } from 'react';
 import { Navbar, FormControl, Form, Button } from 'react-bootstrap';
 
 
-const Searchbar = () => {
+const Searchbar = ({ setArtistSearch }) => {
+
+    //useState to change the state of the artist
+    const [search, setSearch] = useState({
+        artist: ''
+    });
+
+    //this function reads the content of the input field
+    const setState = e => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    //this state manages if an error is produced in the field form
+    const [error, setError] = useState(false);
+    const { artist } = search;
+
+    // Search function 
+    const searchInput = e => {
+        e.preventDefault();
+
+        if (artist.trim() === "") {
+            setError(true);
+            return;
+        }
+        // if the function does not trigger an error (artist is fullfiled), then setError is false and the function triggers setArtistSearch
+        setError(false);
+        setArtistSearch(search.artist)
+    }
+
     return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">iTunes Search</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Form inline>
-                        <FormControl type="text" placeholder="Artist Name" className="mr-sm-2" />
-                        <Button variant="outline-secondary">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">iTunes Search</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Form inline>
+                    <FormControl onSubmit={searchInput}
+                        type="text"
+                        placeholder="Artist Name"
+                        name="artist"
+                        onChange={setState}
+                        value={artist}
+                        className="mr-sm-2"
+                    />
+                    <Button variant="outline-secondary" type="submit">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
