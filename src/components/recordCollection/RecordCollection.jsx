7aab54619acb08460a2be8e92eb6d42939b15255
andre <andre4130@gmail.com>
2020-12-09 18:03:41 +0100

    favourites working

--- src/components/recordCollection/RecordCollection.jsx
+++ src/components/recordCollection/RecordCollection.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Song from './Song';
 import { getMusic } from '../../redux/actions/artists';
-import { CardColumns } from 'react-bootstrap'
+import { CardColumns, CardDeck, CardGroup } from 'react-bootstrap'
 
 const UsersComponent = () => {
     const dispatch = useDispatch();
@@ -16,27 +16,39 @@ const UsersComponent = () => {
 
     const [fav, setFav] = useState([])
 
-    const addFav = ( props ) => {
+    const addFav = (props) => {
+        console.log("add", props)
         let tempArray = fav;
         tempArray.push(props)
-        console.log(tempArray)
         setFav([...fav], [tempArray])
-        // setFav(...fav, props)
-        }
+        console.log("end of add", fav)
+    }
 
-    const removeFav = ( props ) => {
-        console.log('i am remove', props)
+    const removeFav = (props) => {
+        console.log("remove props", props)
+        let array = fav;
+        array.map((item, i) => {
+            console.log("remove", item, i)
+            if (item === props) {
+                array.splice(i, 1);
+                console.log("inside if", item)
+            }
+            setFav([...array]);
+        })
     }
 
     return (
         <div className="container-fluid">
             {loading && <p>Loading...</p>}
             {songs.length > 0 &&
-                <CardColumns>
-                    {songs.map((song, i) => (
-                        <Song className="card" song={song} key={song.id} item={i} fav={fav} addFav={addFav} removeFav={removeFav}/>
-                    ))}
-                </CardColumns>
+                <div className="container-fluid">
+                    <CardColumns>
+                        {songs.map((song, i) => (
+                            <Song className="card" song={song} key={i} item={i} fav={fav} addFav={addFav} removeFav={removeFav} />
+                        ))}
+                    </CardColumns>
+                </div>
+
             }
             {songs.length === 0 && !loading && <p>No users Available</p>}
             {error && !loading && <p>{error}</p>}
--- src/components/recordCollection/Song.jsx
+++ src/components/recordCollection/Song.jsx
@@ -3,17 +3,6 @@ import { Card } from 'react-bootstrap';
 import {IoIosHeart, IoIosHeartEmpty} from 'react-icons/io'
 
 const Song = (props) => {
-
-// const [fav, setFav] = ({
-//     isFav: false
-// })
-
-// onClick={() => setFav(!fav.isFav)}
-
-// const [fav, setFav] = useState([0,1])
-
-
-
     return (
         <Card>
             <Card.Img className="card-img" variant="top" src={props.song.artworkUrl100} />
@@ -22,14 +11,14 @@ const Song = (props) => {
                 <Card.Text>
                     {props.song.collectionName}
             </Card.Text>
-                {props.fav.includes(props.item) ? 
+                {props.fav.includes(props.song.trackId) ? 
                 <IoIosHeart
-                    onClick={() => props.removeFav(props.item)}
+                    onClick={() => props.removeFav(props.song.trackId)}
                     style= {{ color: 'red' }}
                 />
               :
                 <IoIosHeartEmpty
-                    onClick={() => props.addFav(props.item)}
+                    onClick={() => props.addFav(props.song.trackId)}
                     style= {{ color: 'red' }}
                 />
                      }
